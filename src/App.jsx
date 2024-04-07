4dd6a782f9314f47f707f20d7aad29bdbd20eba4
Maxim <kmaxusk@gmail.com>
2024-04-07 10:20:08 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<div>Home page</div>} />
+        <Route path="/movies" element={<div>MoviesPage</div>} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+// '/' – компонент HomePage, домашня сторінка із списком популярних кінофільмів.
+// '/movies' – компонент MoviesPage, сторінка пошуку кінофільмів за ключовим словом.
+// '/movies/:movieId' – компонент MovieDetailsPage, сторінка із детальною інформацією про кінофільм.
+// /movies/:movieId/cast – компонент MovieCast, інформація про акторський склад. Рендериться в нижній частині на сторінці MovieDetailsPage.
+// /movies/:movieId/reviews – компонент MovieReviews, інформація про огляди. Рендериться в нижній частині на сторінці MovieDetailsPage.
+// Якщо користувач зайшов за неіснуючим маршрутом, потрібно показувати компонент NotFoundPage, в якому є посилання Link на домашню сторінку.
